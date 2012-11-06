f24e15d13824d8d3138c0c9fbe86a16afda39a05
John Reese <john@noswap.com>
2012-11-06 15:02:40 -0800

    Add site config file loading and merging

--- nib/config.py
+++ nib/config.py
@@ -2,10 +2,28 @@ from os import path
 
 import nib
 
+def merge(dest, source):
+    """In-place, recursive merge of two dictionaries."""
+    for key in source:
+        if key in dest:
+            if isinstance(dest[key], dict) and isinstance(source[key], dict):
+                merge(dest[key], source[key])
+                continue
+
+        dest[key] = source[key]
+
+    return dest
+
 class Config(dict):
-    def __init__(self):
+    def __init__(self, filename=None):
         cwd = path.abspath(path.dirname(__file__))
-        defaults = nib.yaml.load(path.join(cwd, 'config.defaults'))
+        values = nib.yaml.load(path.join(cwd, 'config.defaults'))
 
-        dict.__init__(self, defaults)
+        if filename is None and path.isfile('config.nib'):
+            filename = 'config.nib'
 
+        if filename is not None:
+            overrides = nib.yaml.load(filename)
+            merge(values, overrides)
+
+        dict.__init__(self, values)
