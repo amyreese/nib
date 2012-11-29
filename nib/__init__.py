e12d34f4c885bf179b84517b64dae66534eeb3eb
John Reese <john@noswap.com>
2012-11-29 15:29:53 -0800

    Catch errors when importing nib during setup.py

--- nib/__init__.py
+++ nib/__init__.py
@@ -1,15 +1,20 @@
-version = '0.4.2'
+version = '0.4.3'
 
 from os import path
 cwd = path.abspath(path.dirname(__file__))
 
-import nib.yaml as yaml
-from nib.config import Config
-from nib.document import Document
-from nib.resource import Resource
-from nib.render import Render, jinja
-from nib.processor import Processor
-from nib.processor import before, after, document, resource, markup
-from nib.build import Build
+try:
+    import nib.yaml as yaml
+    from nib.config import Config
+    from nib.document import Document
+    from nib.resource import Resource
+    from nib.render import Render, jinja
+    from nib.processor import Processor
+    from nib.processor import before, after, document, resource, markup
+    from nib.build import Build
 
-import nib.plugins
+    import nib.plugins
+
+except ImportError:
+    # let's hope this only happens in setup.py until I find a better way
+    pass
